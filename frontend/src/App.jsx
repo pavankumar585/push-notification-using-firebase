54702efefc909b42f6e29e031707c0fac103119e
Pavan Kumar <pavanmaddala16@gmail.com>
2023-07-17 21:42:58 +0530

    WIP

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <Routes>
+      <Route index element={<Navigate to="/register" />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  );
 }
 
 export default App;
